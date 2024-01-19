a30b10b603e70ebff54998ccd48da84a9944092a
Mauricio Crecencio <65139661+mauriciocrecencio@users.noreply.github.com>
2024-01-19 18:19:05 -0300

    create login background and configure eslint + prettier

---
+++ src/app/login/layout.tsx
@@ -0,0 +1,18 @@
+import SwimmingPool from '@/app/_assets/_images/swimming-pool.png';
+import Image from 'next/image';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="fixed left-0 top-0 -z-50 h-screen w-screen">
+        <Image
+          src={SwimmingPool}
+          alt="A big pool in background"
+          quality="100"
+          layout="fill"
+        />
+      </div>
+      {children}
+    </>
+  );
+}
