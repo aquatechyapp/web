faff8ba7129d152ccefe8dc43c5f8d8c16cb68c0
Mauricio Crecencio <65139661+mauriciocrecencio@users.noreply.github.com>
2024-03-29 16:21:49 -0300

    desabilita todos os outros weekdays a não ser o escolhido

--- src/app/_components/ui/date-picker.tsx
+++ src/app/_components/ui/date-picker.tsx
@@ -12,6 +12,7 @@ import {
   PopoverContent,
   PopoverTrigger
 } from '@/app/_components/ui/popover';
+import { useWeekdayContext } from '@/context/weekday';
 
 type Props = {
   placeholder: string;
@@ -19,8 +20,38 @@ type Props = {
   disabledWeekdays?: number[];
 };
 
-export function DatePicker({ placeholder, onChange, disabledWeekdays }: Props) {
+export function DatePicker({ placeholder, onChange }: Props) {
   const [date, setDate] = React.useState<Date>();
+  const { selectedWeekday } = useWeekdayContext();
+
+  const disabledWeekdays = [0, 1, 2, 3, 4, 5, 6];
+  let selectedWeekdayIndex = 0;
+
+  switch (selectedWeekday) {
+    case 'SUNDAY':
+      selectedWeekdayIndex = 0;
+      break;
+    case 'MONDAY':
+      selectedWeekdayIndex = 1;
+      break;
+    case 'TUESDAY':
+      selectedWeekdayIndex = 2;
+      break;
+    case 'WEDNESDAY':
+      selectedWeekdayIndex = 3;
+      break;
+    case 'THURSDAY':
+      selectedWeekdayIndex = 4;
+      break;
+    case 'FRIDAY':
+      selectedWeekdayIndex = 5;
+      break;
+    case 'SATURDAY':
+      selectedWeekdayIndex = 6;
+      break;
+  }
+
+  disabledWeekdays.splice(selectedWeekdayIndex, 1);
 
   React.useEffect(() => {
     if (date) {
