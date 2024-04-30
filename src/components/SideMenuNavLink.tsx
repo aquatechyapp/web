53c8f8b5c75425fb2e0bb176ecd6bed1cd8b2825
jeymes <jsvpn2017@gmail.com>
2024-04-30 20:31:08 -0300

    alterando acordion

--- src/components/SideMenuNavLink.tsx
+++ src/components/SideMenuNavLink.tsx
@@ -4,9 +4,9 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/
 
 type Props = {
   Icon: React.ElementType;
-  text: string;
-  href: string;
-  submenu: string;
+  text: any;
+  href: any;
+  submenu: any;
 };
 
 export default function SideMenuNavLink({ Icon, text, href, submenu }: Props) {
@@ -14,43 +14,58 @@ export default function SideMenuNavLink({ Icon, text, href, submenu }: Props) {
   pathname = pathname.split('/')[1];
 
   const isActive = href === '/' + pathname;
+
   return (
     <Link
-      href={href !== '/clients' ? href : ''}
-      className={`flex w-full p-2 items-center justify-center   ${isActive && 'border-r-4 border-blue-500 bg-gray-800 '}`}
+      href={href}
+      // style={{ borderWidth: 1 }}
+      className={`flex w-full p-2 items-start justify-start  ${isActive && 'border-r-4 border-blue-500 bg-gray-800 '}`}
     >
-      <div className="flex w-[80%] items-center ">
-        <div className="mr-4">
-          <Icon
-            className={`opacity-90 ${isActive ? 'text-blue-500' : 'text-gray-300'}`}
-            height={24}
-            width={24}
-          />
-        </div>
-        {!submenu ? <div className="w-full  text-base font-medium leading-none text-slate-50">
-          {text}
-        </div>
-          :
+      <div className="flex items-center w-full"
+      >
 
-          <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-              <AccordionTrigger className="w-full  text-base font-medium leading-none text-slate-50">
-                {text}
-              </AccordionTrigger>
+        {!submenu ?
+          <>
+            <div className="mr-4" >
+              <Icon
+                className={`opacity-90 ${isActive ? 'text-blue-500' : 'text-gray-300'}`}
+                height={24}
+                width={24}
+              />
+            </div>
+            <div className="w-full py-4 text-base font-medium leading-none text-slate-50" >
+              {text}
+            </div>
+          </>
 
+          : <>
 
-              {Object.entries(submenu).map(([key, subItem]) => (
-                    <Link key={key} href={subItem.href}>
-                      <AccordionContent className="w-full text-ls font-medium leading-none text-gray-500">
-                        {subItem.text}
-                      </AccordionContent>
-                    </Link>
-              ))}
-            </AccordionItem>
-          </Accordion>
-
+            <Accordion type="single" collapsible className="w-[100%]">
+              <AccordionItem value="item-1" style={{ borderBottom: 'none'}} >
+                <AccordionTrigger style={{ textDecoration: 'none'}} 
+                className="w-full  text-base font-medium leading-none text-slate-50">
+                  <div className="flex justify-start items-start w-full">
+                    <Icon
+                      className={`opacity-90 ${isActive ? 'text-blue-500' : 'text-gray-300'} mr-4`}
+                      height={24}
+                      width={24}
+                    />
+                  {text}
+                  </div>
+                </AccordionTrigger>
+                {Object.entries(submenu).map(([key, subItem]) => (
+                  <Link key={key} href={subItem.href}>
+                    <AccordionContent className="text-ls font-medium leading-none text-gray-500">
+                      {subItem.text}
+                    </AccordionContent>
+                  </Link>
+                ))}
+              </AccordionItem>
+            </Accordion>
+          </>
         }
       </div>
+
     </Link>
 
   );
