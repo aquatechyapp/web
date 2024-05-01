946d10920758919be7460f932e401bbf53501d6e
jeymes <jsvpn2017@gmail.com>
2024-04-30 22:39:41 -0300

    created logic in menu

--- src/components/SideMenuNav.tsx
+++ src/components/SideMenuNav.tsx
@@ -1,22 +1,17 @@
 'use client';
 
 import { routes } from '@/constants';
+
 import SideMenuNavLink from './SideMenuNavLink';
 
 export default function SideMenuNav() {
-
   return (
     <aside className="col-span-1  bg-gray-900">
       <div className="inline-flex w-full flex-col items-start justify-start gap-4 bg-gray-900 shadow-inner">
         <div className="flex  flex-col items-center justify-start gap-2.5 self-stretch px-6 pb-7 pt-6">
           <div className="inline-flex items-center justify-center gap-3 self-stretch">
             <div className="shrink grow basis-0 self-stretch text-center">
-              <span className="text-xl font-semibold  text-gray-50">
-                Aquatechy
-              </span>
-              <span className="text-xl font-semibold  text-blue-500">
-                .
-              </span>
+              <img src="./logoHor.png" alt="" className="w-full" />
             </div>
           </div>
         </div>
--- src/components/SideMenuNavLink.tsx
+++ src/components/SideMenuNavLink.tsx
@@ -19,51 +19,51 @@ export default function SideMenuNavLink({ Icon, text, href, submenu }: Props) {
     <Link
       href={href}
       // style={{ borderWidth: 1 }}
-      className={`flex w-full p-2 items-start justify-start  ${isActive && 'border-r-4 border-blue-500 bg-gray-800 '}`}
+      className={`flex w-full px-2 items-start justify-start  ${isActive && 'border-r-4 border-blue-500 bg-gray-800 '}`}
     >
       <div className="flex items-center w-full"
       >
-
-        {!submenu ?
-          <>
-            <div className="mr-4" >
+            {!submenu ?
+            <>
+             <div className="mr-4 py-4" >
               <Icon
                 className={`opacity-90 ${isActive ? 'text-blue-500' : 'text-gray-300'}`}
                 height={24}
                 width={24}
               />
             </div>
-            <div className="w-full py-4 text-base font-medium leading-none text-slate-50" >
+            <div className="w-full text-base font-medium leading-none text-slate-50" >
               {text}
             </div>
-          </>
-
-          : <>
-
-            <Accordion type="single" collapsible className="w-[100%]">
+            </>
+            :
+            <Accordion  collapsible type="single" className="w-[100%]">
               <AccordionItem value="item-1" style={{ borderBottom: 'none'}} >
-                <AccordionTrigger style={{ textDecoration: 'none'}} 
-                className="w-full  text-base font-medium leading-none text-slate-50">
-                  <div className="flex justify-start items-start w-full">
+                <AccordionTrigger style={{ textDecoration: 'none' }}
+                  className="w-full  text-base font-medium leading-none text-slate-50">
+                  <div className="flex justify-start items-start w-[20%]">
                     <Icon
                       className={`opacity-90 ${isActive ? 'text-blue-500' : 'text-gray-300'} mr-4`}
                       height={24}
                       width={24}
                     />
-                  {text}
+                  </div>
+                  <div className="flex justify-start items-start w-full">
+                    {text}
                   </div>
                 </AccordionTrigger>
                 {Object.entries(submenu).map(([key, subItem]) => (
                   <Link key={key} href={subItem.href}>
                     <AccordionContent className="text-ls font-medium leading-none text-gray-500">
-                      {subItem.text}
+                      <div className="flex justify-start items-start w-[50%] ml-10">
+                        {subItem.text}
+                      </div>
                     </AccordionContent>
                   </Link>
                 ))}
               </AccordionItem>
             </Accordion>
-          </>
-        }
+            }
       </div>
 
     </Link>
