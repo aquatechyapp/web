fee2d5f84caf063d5915870efcb5d34022f7d16f
Mauricio Crecencio <65139661+mauriciocrecencio@users.noreply.github.com>
2024-05-02 17:43:10 -0300

    ajusta interfaces pois tinha duas Pool, a outra está dentro de Assignments

--- src/interfaces/Client.ts
+++ src/interfaces/Client.ts
@@ -1,10 +1,10 @@
-import { Assignment } from './Assignments';
-import { Service } from './Service';
+import { Pool } from './Assignments';
 
 export interface Client {
   id: string;
   address: string;
   city: string;
+  company: string;
   customerCode: string;
   createdAt: string;
   deactivatedAt: string;
@@ -20,31 +20,7 @@ export interface Client {
   zip: string;
   pools: Pool[];
   lastServiceDate: string;
-}
-
-export interface Pool {
-  coords: Coords;
-  id: string;
-  address: string;
-  animalDanger: boolean;
-  city: string;
-  clientOwnerId: string;
-  createdAt: string;
-  deactivatedAt: string;
-  enterSide: string;
-  isActive: boolean;
-  lockerCode: string;
-  montlyPayment: string;
-  name: string;
-  notes: string;
-  poolType: string;
-  state: string;
-  updatedAt: string;
-  userOwnerId: string;
-  zip: string;
-  photos: string[];
-  assignments: Assignment[];
-  services: Service[];
+  clientType: 'Residential' | 'Commercial';
 }
 
 export interface Coords {
