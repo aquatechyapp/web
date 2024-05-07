dfbd233491ffad9be72345195f01f0f460deba11
jeymes <jsvpn2017@gmail.com>
2024-05-07 11:39:40 -0300

    ajuste na task 32 e 30

--- src/app/(unauthorized)/layout.tsx
+++ src/app/(unauthorized)/layout.tsx
@@ -3,18 +3,26 @@ import Image from 'next/image';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen">
-      <div className="fixed left-0 top-0 -z-50 h-screen w-screen">
+    <div
+      className="h-screen">
+
+      <div
+        className="fixed left-0 top-0 -z-50 h-screen w-screen">
+
         <Image
           className="h-screen w-screen object-cover"
           src={SwimmingPool}
           alt="A big pool in background"
           quality="100"
         />
+
       </div>
-      <div className="h flex h-[100%] max-h-[100vh] items-center justify-center">
+
+      <div className="h flex h-[100%] max-h-[100vh]
+      items-center justify-center">
         {children}
       </div>
+
     </div>
   );
 }
