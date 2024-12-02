ffed88979ef6036ef27cd43fb52231d16eb11e0a
Kawan R. Strelow <admin@aquatechyapp.com>
2024-12-01 20:20:35 -0500

    fixed a test

--- src/__tests__/pages/clients/import/page.test.tsx
+++ src/__tests__/pages/clients/import/page.test.tsx
@@ -7,6 +7,15 @@ vi.mock('useMutation', () => ({
   mutate: vi.fn()
 }));
 
+vi.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: vi.fn(), // Mock router.push
+    replace: vi.fn(), // Mock router.replace
+    prefetch: vi.fn(), // Mock router.prefetch
+    pathname: '/clients/import' // Mock pathname
+  })
+}));
+
 describe('/clients/import', () => {
   const wrapper = render(
     <ReactQueryMocked>
