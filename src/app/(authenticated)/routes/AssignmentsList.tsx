3889ba5ac7804fa809b0e2c40f0f578ea09e2f05
Kawan R. Strelow <admin@aquatechyapp.com>
2024-12-10 15:59:47 -0500

    fixed assignment bug

--- src/app/(authenticated)/routes/AssignmentsList.tsx
+++ src/app/(authenticated)/routes/AssignmentsList.tsx
@@ -17,6 +17,7 @@ import {
 } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
 import { format } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
 import { useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import { MdDragIndicator } from 'react-icons/md';
@@ -156,9 +157,11 @@ export function AssignmentItem({ id, assignment, shouldPermitChangeOrder }: Assi
 
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
 
-  const startsOn = format(new Date(assignment.startOn), 'LLL, do, y');
-  const endsAfter =
-    new Date(assignment.endAfter).getFullYear() > 2100 ? 'No end' : format(new Date(assignment.endAfter), 'LLL, do, y');
+  const zonedStartOn = toZonedTime(assignment.startOn, 'UTC');
+  const startsOn = format(zonedStartOn, 'LLL, do, y');
+
+  const zonedEndAfter = toZonedTime(assignment.endAfter, 'UTC');
+  const endsAfter = zonedEndAfter.getFullYear() > 2100 ? 'No end' : format(zonedEndAfter, 'LLL, do, y');
 
   const isOnlyAt = startsOn === endsAfter;
 
