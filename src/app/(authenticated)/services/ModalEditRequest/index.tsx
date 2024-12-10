023daf3b65e1eaa19b7bf7facc031cc31c801c52
jeymes <jsvpn2017@gmail.com>
2024-12-09 21:34:08 -0300

    created modal adjust

--- src/app/(authenticated)/services/ModalEditRequest/index.tsx
+++ src/app/(authenticated)/services/ModalEditRequest/index.tsx
@@ -28,46 +28,47 @@ export function ModalEditRequest({ request, open, setOpen }: Props) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="max-h-[90vh] max-w-4xl overflow-y-auto p-6">
-        <div className="flex">
-          <ListChecks className="mr-2" />
-          <DialogTitle className="text-lg font-semibold">
-            {`${request?.createdAt || 'Unknown'}. (made by ${request?.doneByUser?.firstName || 'Unknown'} ${request?.doneByUser?.lastName || 'Unknown'})`}
-          </DialogTitle>
+        <div>
+          <div className="flex">
+            <ListChecks className="mr-2" />
+            <DialogTitle className="text-lg font-semibold">
+              {`${request?.createdAt || 'Unknown'}. (made by ${request?.doneByUser?.firstName || 'Unknown'} ${request?.doneByUser?.lastName || 'Unknown'})`}
+            </DialogTitle>
+          </div>
+          <p className="text-sm text-gray-500">
+            {request.pool.zip}, {request.pool.state}, {request.pool.city}, {request.pool.address}
+          </p>
         </div>
 
-        <p className="text-sm text-gray-500">
-          {request.pool.zip}, {request.pool.state}, {request.pool.city}, {request.pool.address}
-        </p>
-
         {/* Chemical Readings and Checklist */}
-        <div className="mt-4 grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-4">
           <div>
             <h2 className="text-center text-lg font-bold text-gray-700">Chemical readings</h2>
             <table className="w-full text-sm">
               <tbody>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">Chlorine</td>
-                  <td className="py-2 pr-4">{request.chlorine}</td>
+                  <td className="py-2 pr-4 text-gray-700">Chlorine</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.chlorine}</td>
                 </tr>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">P.H</td>
-                  <td className="py-2 pr-4">{request.ph}</td>
+                  <td className="py-2 pr-4 text-gray-700">P.H</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.ph}</td>
                 </tr>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">Salt</td>
-                  <td className="py-2 pr-4">{request.salt}</td>
+                  <td className="py-2 pr-4 text-gray-700">Salt</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.salt}</td>
                 </tr>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">Alkalinity</td>
-                  <td className="py-2 pr-4">{request.alkalinity}</td>
+                  <td className="py-2 pr-4 text-gray-700">Alkalinity</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.alkalinity}</td>
                 </tr>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">Cyan. Acid</td>
-                  <td className="py-2 pr-4">{request.cyanAcid}</td>
+                  <td className="py-2 pr-4 text-gray-700">Cyan. Acid</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.cyanAcid}</td>
                 </tr>
                 <tr className="border-b">
-                  <td className="py-2 pr-4">Calcium</td>
-                  <td className="py-2 pr-4">{request.calcium}</td>
+                  <td className="py-2 pr-4 text-gray-700">Calcium</td>
+                  <td className="py-2 pr-4 text-gray-700">{request.calcium}</td>
                 </tr>
               </tbody>
             </table>
@@ -78,7 +79,7 @@ export function ModalEditRequest({ request, open, setOpen }: Props) {
               <tbody>
                 {checklistItems.map((item, index) => (
                   <tr key={index} className="border-b">
-                    <td className="py-2 pr-4">{item.name}</td>
+                    <td className="py-2 pr-4 text-gray-700">{item.name}</td>
                     {item.completed ? (
                       <CheckCircleIcon className="h-5 w-5 text-gray-700" />
                     ) : (
@@ -92,45 +93,48 @@ export function ModalEditRequest({ request, open, setOpen }: Props) {
         </div>
 
         {/* Photos */}
-        <div className="mt-4">
-          {/* <h2 className="text-center text-lg font-bold text-gray-700">Photos</h2> */}
-          <div className="mt-2 grid grid-cols-3 gap-2">
-            {request?.photos?.map((photo: string, index: number) => (
-              <div key={index} className="relative aspect-square">
-                <Image src={photo} alt={`Photo ${index + 1}`} layout="fill" className="rounded object-cover" />
-              </div>
-            ))}
+        {request?.photos.length > 0 && (
+          <div>
+            {/* <h2 className="text-center text-lg font-bold text-gray-700">Photos</h2> */}
+            <div className="grid grid-cols-3 gap-2">
+              {request?.photos?.map((photo: string, index: number) => (
+                <div key={index} className="relative aspect-square">
+                  <Image src={photo} alt={`Photo ${index + 1}`} layout="fill" className="rounded object-cover" />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Chemicals Spent */}
-        <div className="mt-4">
+        <div>
           <h2 className="text-center text-lg font-bold text-gray-700">Chemicals spent</h2>
           <table className="w-full text-sm">
             <tbody>
               <tr className="border-b">
-                <td className="py-2 pr-4">Liquid Chlorine (gallon)</td>
-                <td className="py-2">{request?.chlorineSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Liquid Chlorine (gallon)</td>
+                <td className="py-2 text-gray-700">{request?.chlorineSpent || 'N/A'}</td>
               </tr>
               <tr className="border-b">
-                <td className="py-2 pr-4">Muriatic Acid (gallon)</td>
-                <td className="py-2">{request?.acidSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Muriatic Acid (gallon)</td>
+                <td className="py-2 text-gray-700">{request?.acidSpent || 'N/A'}</td>
               </tr>
               <tr className="border-b">
-                <td className="py-2 pr-4">Salt (bags)</td>
-                <td className="py-2">{request?.saltSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Salt (bags)</td>
+                <td className="py-2 text-gray-700">{request?.saltSpent || 'N/A'}</td>
               </tr>
               <tr className="border-b">
-                <td className="py-2 pr-4">Shock</td>
-                <td className="py-2">{request?.shockSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Shock</td>
+                <td className="py-2 text-gray-700">{request?.shockSpent || 'N/A'}</td>
               </tr>
               <tr className="border-b">
-                <td className="py-2 pr-4">Tablet</td>
-                <td className="py-2">{request?.tabletSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Tablet</td>
+
+                <td className="py-2 text-gray-700">{request?.tabletSpent || 'N/A'}</td>
               </tr>
               <tr className="border-b">
-                <td className="py-2 pr-4">Phosphate Remover (gallon)</td>
-                <td className="py-2">{request?.phosphateSpent || 'N/A'}</td>
+                <td className="py-2 pr-4 text-gray-700">Phosphate Remover (gallon)</td>
+                <td className="py-2 text-gray-700">{request?.phosphateSpent || 'N/A'}</td>
               </tr>
             </tbody>
           </table>
@@ -144,9 +148,11 @@ export function ModalEditRequest({ request, open, setOpen }: Props) {
           <Button className="w-full" type="submit">
             Download report
           </Button>
-          <Button variant="outline" className="w-full" type="submit">
-            Update Request
-          </Button>
+          {request?.photos.length > 0 && (
+            <Button variant="outline" className="w-full" type="submit">
+              Download photos
+            </Button>
+          )}
         </div>
       </DialogContent>
     </Dialog>
