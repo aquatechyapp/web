6296737461728801ad4c025bc4fc676ebda77882
Kawan R. Strelow <admin@aquatechyapp.com>
2025-03-05 19:21:16 -0500

    Implement advanced filtering and pagination for requests page

--- src/hooks/react-query/requests/getRequests.ts
+++ src/hooks/react-query/requests/getRequests.ts
@@ -1,14 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { clientAxios } from '@/lib/clientAxios';
 import { Request } from '@/ts/interfaces/Request';
 
-export default function useGetRequests() {
-  const { data, isLoading, isSuccess } = useQuery({
-    queryKey: ['requests'],
-    queryFn: async () => {
-      const response = await clientAxios('/requests');
+export interface UseGetRequestsParams {
+  from: string;
+  to: string;
+  status: string | null;
+  category: string | null;
+  clientId: string | null;
+  companyId: string | null;
+  page: number;
+  limit: number;
+}
 
+export default function useGetRequests(params: UseGetRequestsParams) {
+  const queryClient = useQueryClient();
+
+  const query = useQuery({
+    queryKey: ['requests', params],
+    queryFn: async () => {
+      const response = await clientAxios.get('/requests', { params });
       response?.data?.requests.forEach((request: Request) => {
         request.client.fullName = `${request.client.firstName} ${request.client.lastName}`;
       });
@@ -16,5 +28,19 @@ export default function useGetRequests() {
     },
     staleTime: Infinity
   });
-  return { data, isLoading, isSuccess };
+
+  const refetch = async (newParams: UseGetRequestsParams) => {
+    return queryClient.fetchQuery({
+      queryKey: ['requests', newParams],
+      queryFn: async () => {
+        const response = await clientAxios.get('/requests', { params: newParams });
+        response?.data?.requests.forEach((request: Request) => {
+          request.client.fullName = `${request.client.firstName} ${request.client.lastName}`;
+        });
+        return response.data;
+      }
+    });
+  };
+
+  return { ...query, refetch };
 }
