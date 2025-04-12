6868f6827312f8b56d6e6fc81d9235b5d59a7274
Kawan R. Strelow <admin@aquatechyapp.com>
2025-04-12 09:07:28 -0400

    Enhance useUpdatePool hook with user-specific query invalidations
    - Integrated userId retrieval from cookies to invalidate assignments and schedule queries upon successful pool updates.
    - Increased toast notification duration for improved user feedback on pool updates.

--- src/hooks/react-query/pools/updatePool.ts
+++ src/hooks/react-query/pools/updatePool.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { z } from 'zod';
-
+import Cookies from 'js-cookie';  
 import { useToast } from '@/components/ui/use-toast';
 import { clientAxios } from '@/lib/clientAxios';
 import { editPoolSchema } from '@/schemas/pool';
@@ -9,6 +9,7 @@ import { editPoolSchema } from '@/schemas/pool';
 export const useUpdatePool = () => {
   const queryClient = useQueryClient();
   const { toast } = useToast();
+  const userId = Cookies.get('userId');
 
   const { mutate, isPending } = useMutation({
     mutationFn: async ({ data }: { data: z.infer<typeof editPoolSchema> }) =>
@@ -16,8 +17,10 @@ export const useUpdatePool = () => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['pools'] });
       queryClient.invalidateQueries({ queryKey: ['clients'] });
+      queryClient.invalidateQueries({ queryKey: ['assignments', userId] });
+      queryClient.invalidateQueries({ queryKey: ['schedule', userId] });
       toast({
-        duration: 2000,
+        duration: 5000,
         title: 'Pool updated successfully',
         variant: 'success'
       });
