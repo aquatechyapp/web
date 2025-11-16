c4441d166eb54239fee99770d994ae6bf96a57f0
Kawan R. Strelow <admin@aquatechyapp.com>
2025-11-16 09:57:15 -0500

    refactor: remove 'Owner' role from company member management
    - Updated role validation in ModalEditCompanyMember and AddMemberPage to exclude 'Owner' from the available roles.
    - Cleaned up related UI components to reflect the changes in role options.
    - Removed commented-out code in useGetMembersOfAllCompaniesByUserId for better code clarity.

--- src/hooks/react-query/companies/getMembersOfAllCompaniesByUserId.tsx
+++ src/hooks/react-query/companies/getMembersOfAllCompaniesByUserId.tsx
@@ -27,29 +27,6 @@ export default function useGetMembersOfAllCompaniesByUserId(userId: string) {
 
       const members: CompanyMember[] = response.data.members ? response.data.members : [];
 
-      // When the user has no company, the response will be an empty array, so we need to add the user to the members array.
-
-      // if (members.length === 0) {
-      //   members.push({
-      //     id: user.id,
-      //     firstName: user.firstName,
-      //     lastName: user.lastName,
-      //     email: user.email,
-      //     phone: user.phone,
-      //     company: {
-      //       id: '',
-      //       name: '',
-      //     },
-      //     role: 'Owner',
-      //     user: {
-      //       id: user.id,
-      //       email: user.email,
-      //       name: user.name,
-      //       avatar: user.avatar
-      //     }
-      //   });
-      // }
-
       setMembers(members);
 
       return members;
