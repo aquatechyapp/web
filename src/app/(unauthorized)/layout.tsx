b55ed41645b04c016a9dd75998f2494e75850722
Kawan R. Strelow <admin@aquatechyapp.com>
2026-02-28 12:32:27 -0500

    feat: update background image in unauthorized layout
    - Replaced the swimming pool background image with a new Aquatechy-themed background image for improved visual consistency.
    - Removed the old swimming pool image file to streamline asset management.

--- src/app/(unauthorized)/layout.tsx
+++ src/app/(unauthorized)/layout.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image';
 
-import SwimmingPool from '@/assets/images/swimming-pool.png';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen">
       <div className="fixed left-0 top-0 -z-50 h-screen w-screen">
         <Image
           className="h-screen w-screen object-cover"
-          src={SwimmingPool}
-          alt="A big pool in background"
+          src="/images/aquatechy-background.png"
+          alt="Aquatechy background"
           quality="100"
         />
       </div>
